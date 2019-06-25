9baf2554c770917b00f468767a0250ceab073dda
prisoner <540133411@qq.com>
2019-06-25 12:11:26 +0800

    签入代码到主分支

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<div>state：{{this.$store.state.count}}</div>
+		<div>getters:{{this.$store.getters.stateCount}}</div>
+
+		<span @click="reduce">-</span>
+		<span @click="add">+</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	data() {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	},
+	props: {},
+	components: {},
+	mounted() {
+		setTimeout(() => {
+			this.$store.state.count = 2;
+		}, 1000)
+	},
+	created() { },
+	watch: {},
+	methods: {
+		reduce() {
+			this.$store.state.count--
+		},
+		add() {
+			this.$store.state.count++
+		},
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+//使用vuex
+Vue.use(Vuex);
+//创建实例
+const store = new Vuex.Store({
+	state: {
+		count: 1
+	},
+	getters: {
+		stateCount: function (state) {
+			return state.count+3
+		}
+	}
+})
+export default store;
